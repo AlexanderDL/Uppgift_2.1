807e963df6936a4a4a42027663101078de110b56
AlexanderDL <alde09001@utb.vaxjo.se>
2026-09-16 00:11:20 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur långt hoppade Elin?");
+        double elin = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Hur långt hoppade Alma?");
+        double alma = double.Parse(Console.ReadLine());
+
+        double skillnad = elin - alma;
+
+        Console.WriteLine("Elin hoppade " + skillnad + " meter längre.");
+    }
+}
